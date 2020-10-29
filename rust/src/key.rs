5678829426e88944dda70e35506e3cbba96b4b62
rishav4101 <rajkumarrishav4101@gmail.com>
2020-10-29 23:48:58 +0530

    ecdsa included

--- rust/src/key.rs
+++ rust/src/key.rs
@@ -3,32 +3,40 @@ extern crate rand;
 
 use rand::rngs::OsRng;
 use secp256k1::{Secp256k1, Message, SecretKey, PublicKey, Signature};
+// use std::collections::hash_map::DefaultHasher;
+// use std::hash::{Hash, Hasher};
 
-// struct Keypair {
-//     seck: String,
-//     pubk: String,
-// }
-
-struct Address {
-    // msg: String,
+pub struct Address {
     seck: String,
-    pubk: String,
+    // pubk: String,
 }
 
+
 impl Address {
+
+    // fn calculate_hash<T: Hash>(t: &T) -> u8 {
+    //     let mut s = DefaultHasher::new();
+    //     t.hash(&mut s);
+    //     s.finish()
+    // }
+
+    pub fn f(s: &[u8]) -> &[u8] {
+        &s
+    }
 
     pub fn gen_key() -> Address {
 
         let secp = Secp256k1::new();
         let mut rng = OsRng::new().expect("OsRng");
-        let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+        let (secret_key, _public_key) = secp.generate_keypair(&mut rng);
 
-        Address { seck: secret_key.to_string(), pubk: public_key.to_string() }
+        Address { seck: secret_key.to_string() }
     }
 
     pub fn sign_msg(&self, msg: String) -> String {
         let secp = Secp256k1::new();
-        let message = Message::from_slice(&msg.as_bytes()).expect("32 bytes");
+        println!("{:?}", &msg.as_bytes());
+        let message = Message::from_slice(&Address::f(&msg.as_bytes())).expect("32 bytes");
         let secret_key = SecretKey::from_slice(&self.seck.as_bytes()).expect("32 bytes, within curve order");
         let sig = secp.sign(&message, &secret_key);
 
@@ -42,7 +50,15 @@ impl Address {
         let public_key = PublicKey::from_secret_key(&secp, &secret_key);
         let signature = Signature::from_compact(&sig.as_bytes()).expect("64 bytes");
         assert!(secp.verify(&message, &signature, &public_key).is_ok());
+        println!("{:?}", secret_key.to_string());
     }
+}
+
+pub fn key() {
+    let add = Address::gen_key();
+    let sig = Address::sign_msg(&add, ("rishav raj").to_string());
+    Address::verif_msg(&add, ("rishav raj").to_string(), sig);
+
 }
 
 // pub fn key() {
